684960b3feb8828fff88e6f4e749d8bdaac01702
dcastrog2uniandes <d.castrog2@uniandes.edu.co>
2022-10-15 00:44:16 -0500

    Se crear microservicio convertirArchivo y microservicio EnviarCorreo

---
+++ ConvertirArchivo/ogg/conversor.py
@@ -0,0 +1,10 @@
+from pydub import AudioSegment
+
+def convert_audio_to_ogg(filepath:str,output_file:str, format):
+    if format == '.wav':
+        sound = AudioSegment.from_wav(filepath)
+    if format == '.mp3':
+        sound = AudioSegment.from_mp3(filepath)
+    
+    sound.export(output_file + '.ogg', format='ogg')
+    return 
