31bd69326d2746ddd65d17c2053115a666f659b8
miltong24 <miltong24@hotmail.com>
2022-10-22 11:23:16 -0500

    Se eliminan archivos que no sirven y se actualiza la generacion del archivo

--- ConvertirArchivo/ogg/conversor.py
+++ ConvertirArchivo/ogg/conversor.py
@@ -1,6 +1,7 @@
 import sys
 from pydub import AudioSegment
 import datetime
+import os
 
 class ConversorOGG:
     def convert_audio_to_ogg(self,filepath:str,output_file:str, format):
@@ -11,7 +12,7 @@ class ConversorOGG:
                 sound = AudioSegment.from_mp3(filepath)
             
             sound.export(output_file, format='ogg')
-            print('Se proceso el archivo: ',output_file, 'hora: ', datetime.datetime.now())
+            print('Se proceso el archivo: ',output_file, 'hora: ', datetime.datetime.now(), 'tamanio:', os.path.getsize(filepath))
             sys.stdout.flush()
             return True
         except:
--- ConvertirArchivo/wav/conversor.py
+++ ConvertirArchivo/wav/conversor.py
@@ -1,6 +1,7 @@
 import sys
 from pydub import AudioSegment
 import datetime
+import os
 
 class ConversorWAV:
     def convert_audio_to_wav(self,filepath:str,output_file:str,format:str):
@@ -11,7 +12,7 @@ class ConversorWAV:
                 sound = AudioSegment.from_ogg(filepath)
             
             sound.export(output_file, format='wav')
-            print('Se proceso el archivo: ',output_file, 'hora: ', datetime.datetime.now())
+            print('Se proceso el archivo: ',output_file, 'hora: ', datetime.datetime.now(), 'tamanio:', os.path.getsize(filepath))
             sys.stdout.flush()
             return True
         except:
