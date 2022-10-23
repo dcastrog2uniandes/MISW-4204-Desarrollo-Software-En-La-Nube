f737f1497ab3329ca4f884984c85997e2b5be923
miltong24 <miltong24@hotmail.com>
2022-10-22 19:28:39 -0500

    Se crea script para crear archivos de musica

---
+++ crear_file.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for i in {1..660}
+do
+    cp Archivos/ArchivoCliente/Daft_Punk_One_More_Time.mp3 Archivos/ArchivoCliente/Daft_Punk_One_More_Time_$i.mp3
+
+    echo ../Archivos/ArchivoCliente/Daft_Punk_One_More_Time_$i.mp3,.wav >> JmeterTest/tareasMasivo.cvs
+done
