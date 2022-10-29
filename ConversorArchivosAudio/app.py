955e4c3a1a3472b319d71059fe1cd3a71474fdf4
miltong24 <miltong24@hotmail.com>
2022-10-28 21:22:07 -0500

    Se actualiza la conexion al servidor de mysql cloud

--- ConversorArchivosAudio/app.py
+++ ConversorArchivosAudio/app.py
@@ -12,12 +12,34 @@ from login.login import Login
 from actualizarTarea.actualizatarea import ActualizarTarea
 from recuperarArchivo.recuperarArchivo import RecuperarArchivo
 from prometheus_flask_exporter import RESTfulPrometheusMetrics
+import os
 
 app = Flask(__name__)
 metrics = RESTfulPrometheusMetrics.for_app_factory()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///conversorAudio.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ISCONTAINER = os.environ.get('ISCONTAINER', None)
+PASSWORD = os.environ.get('PASSWORD', None)
+PUBLIC_IP_ADDRESS = os.environ.get('PUBLIC_IP_ADDRESS', None)
+DBNAME = os.environ.get('DBNAME', None)
+PROJECT_ID = os.environ.get('PROJECT_ID', None)
+INSTANCE_NAME = os.environ.get('INSTANCE_NAME', None)
+
+if ISCONTAINER is None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:{}@{}/{}".format("admin123456", "127.0.0.1", "conversor")
+    # PASSWORD="admin123456"
+    # PUBLIC_IP_ADDRESS="34.27.228.33"
+    # DBNAME="conversorAudio"
+    # PROJECT_ID="grupo4-cloud-366900:us-central1:bd-conversor-audio"
+    # INSTANCE_NAME="bd-conversor-audio"
+    # app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///conversorAudio.db'
+
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'frase-secreta'
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
