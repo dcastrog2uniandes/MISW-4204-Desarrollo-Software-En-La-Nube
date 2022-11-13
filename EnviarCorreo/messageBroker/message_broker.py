d315d26a6755831b722ba3cf700d8a4c925adc60
miltong24 <miltong24@hotmail.com>
2022-11-12 20:37:20 -0500

    Se agrega cambio para alidar llamado de kafka

--- EnviarCorreo/messageBroker/message_broker.py
+++ EnviarCorreo/messageBroker/message_broker.py
@@ -4,20 +4,19 @@ from EnviarCorreo.send_email import EnviarNotificacion
 import os
 
 class KafkaConsumer():
-    server = os.environ.get('SERVER_KAFKA', None)
-    if server == None:
-        server = '10.128.0.2:9092'
-
-    consumer = KafkaConsumer(
-        'Notificar',
-        bootstrap_servers = [server],
-        value_deserializer=lambda m: loads(m.decode('utf-8')),
-        auto_offset_reset='earliest'
-    )
-    
     def enviarNotificacion(self):
-        notificador = EnviarNotificacion()
-
-        for n in self.consumer:
+        server = os.environ.get('SERVER_KAFKA', None)
+        if server == None:
+            server = 'localhost:9092'
+        
+        consumer = KafkaConsumer(
+            'Notificar',
+            bootstrap_servers = [server],
+            value_deserializer=lambda m: loads(m.decode('utf-8')),
+            auto_offset_reset='earliest',
+            auto_commit_interval_ms=1000
+        )
+        for n in consumer:
+            notificador = EnviarNotificacion()
             notificador.send_email_notification(n.value)
             
