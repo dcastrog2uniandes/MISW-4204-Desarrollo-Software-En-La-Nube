0a307533180a6c1f296b0a2b4bfee5252011c1b2
mcgomeztuniandes <mc.gomezt1@uniandes.edu.co>
2022-11-13 15:00:30 -0500

    Se actualiza para que la deje en la carpeta compartida

--- crear_file.sh
+++ crear_file.sh
@@ -2,7 +2,7 @@
 
 for i in {1..150}
 do
-    cp Archivos/ArchivoCliente/Daft_Punk_One_More_Time.mp3 Archivos/ArchivoCliente/Daft_Punk_One_More_Time_$i.mp3
+    cp Archivos/ArchivoCliente/Daft_Punk_One_More_Time.mp3 /nfs/Archivos/ArchivoCliente/Daft_Punk_One_More_Time_$i.mp3
 
     echo ../Archivos/ArchivoCliente/Daft_Punk_One_More_Time_$i.mp3,.wav >> JmeterTest/tareasMasivo.cvs
 done
